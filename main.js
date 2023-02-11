2a83aa4738d28ced4a6ff113aeaddf31a84cee78
euaisa <isapereira.da@bellevuecollege.edu>
2023-02-10 22:42:42 -0800

    added functionality

--- main.js
+++ main.js
@@ -1,18 +1,158 @@
-//----------------- Product search box --------------//
-const search = () => {
-    const searchText = document.getElementById("search").value;
-    const productDivs = document.querySelectorAll(".product");
-
-    productDivs.forEach((productDiv) => {
-        let productName = productDiv.querySelectorAll(".shop-item-title")[0].textContent;
-
-        if (productName.toUpperCase().indexOf(searchText.toUpperCase()) > -1) {
-            productDiv.style.display = "";
-        }
-        else {
-            productDiv.style.display = "none";
-        }
-
-    });
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
 }
-//----------------- Product search box --------------//
+
+// Cart functionality:
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger');
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button');
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+
+    alert('Item was added.')
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
+
+
+
+
+// Constructor function for creating the Review Objects:
+
+function Review(name, rating, comment) {
+    this.name = name;
+    this.rating = rating;
+    this.comment = comment;
+  }
+
+  var reviews = [];
+
+  function displayReviews() {
+    var container = document.getElementById("reviewsContainer");
+    var html = "<ul class='reviews-list'>";
+  
+    for (var i = 0; i < reviews.length; i++) {
+      var review = reviews[i];
+      html += "<li><h3>" + review.name + "</h3><p>" + review.comment + "</p><p>Rating: " + review.rating + "/5</p></li>";
+    }
+  
+    html += "</ul>";
+    container.innerHTML = html;
+  
+    $(".reviews-list").listview();
+  }
+
+
+
+
+  
+//   Adding new reviews to the DOM:
+
+  var form = document.getElementById("reviewForm");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById("nameInput").value;
+    var rating = document.getElementById("ratingInput").value;
+    var comment = document.getElementById("commentInput").value;
+
+    var review = new Review(name, rating, comment);
+    reviews.push(review);
+
+  });
+
+  var button = document.getElementById("displayButton");
+  button.addEventListener("click", displayReviews);
